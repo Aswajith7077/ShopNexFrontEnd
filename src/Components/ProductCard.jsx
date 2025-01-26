12cc89d24a8829e0be9fda672e068ecea51eaccc
Aswajith S <aswajith707@gmail.com>
2025-01-26 23:15:25 +0530

    prod

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -34,11 +34,11 @@ const ProductCard = ({product}) => {
             className="w-full h-[10rem]"
           />
         </div>
-        <h1 className="text-md font-[lato] font-bold mx-4 mt-3 ">
-          {product.title.slice(0,10)}
+        <h1 className="text-lg font-[lato] font-bold mx-4 mt-3 ">
+          {product.title.split(' ').slice(0,2) + '...'}
         </h1>
         <p className="text-sm font-[lato] font-gray-600 font-semibold mt-1 mx-4">
-          {product.description.slice(0,16)}
+          {product.brand}
         </p>
         <div className="flex flex-row justify-between mb-1 items-center">
           <div className="flex flex-row mx-2 ">
