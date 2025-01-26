6371bfd91a9b48b2db95f757e02aee686ca1f41d
Aswajith S <aswajith707@gmail.com>
2025-01-26 23:07:55 +0530

    temps

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -27,18 +27,18 @@ const ProductCard = ({product}) => {
         <div className="bg-white rounded-[20px] p-10 shadow-md">
           <img
             src={
-              product.PRODUCT_LINK ? 
-              product.PRODUCT_LINK:
+              product.image_url ? 
+              product.image_url:
               "https://firebasestorage.googleapis.com/v0/b/e-commerce-31af1.appspot.com/o/FleeceBlanket.png?alt=media&token=06fa7532-0b90-4aca-bf43-1cde9d6386df"
             }
             className="w-full h-[10rem]"
           />
         </div>
         <h1 className="text-xl font-[lato] font-bold mx-4 mt-3 ">
-          {product.PRODUCT_NAME}
+          {product.title}
         </h1>
         <p className="text-sm font-[lato] font-gray-600 font-semibold mt-1 mx-4">
-          {product.PRODUCT_DESC}
+          {product.description.slice(0,16)}
         </p>
         <div className="flex flex-row justify-between mb-1 items-center">
           <div className="flex flex-row mx-2 ">
@@ -54,7 +54,7 @@ const ProductCard = ({product}) => {
           </div>
           <div className="flex flex-col ">
             <h1 className="font-[manrope] text-2xl mr-4 font-bold text-right ">
-              {`$${product.PRODUCT_PRICE}`}
+              {`$${product.final_price}`}
             </h1>
             <h1 className="font-[manrope] mr-4 font-bold text-right line-through ">
               
